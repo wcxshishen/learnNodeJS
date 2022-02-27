19a067c8b01e25fa163b49758a1a59c21a7099bb
wcxshishen <393584601@qq.con>
2022-02-28 00:53:11 +0800

    try catch 捕获异常方法

--- 2022-02-22/Express/05-async和await版本.js
+++ 2022-02-22/Express/05-async和await版本.js
@@ -6,6 +6,26 @@ const app = express();
 const port = 3000;
 
 app.get("/get_data", (req, res)=>{
+    let Students = db.model("students");
+    let result
+    (async function(){
+        try{
+            result = await new Promise((reslove,reject) => {
+                Students.find("age > 50",(err, data) => {
+                    if(err){
+                        reject(err);
+                        return;
+                    }
+                    reslove(data);
+                })
+            })
+        }catch(err){
+            console.log(err);
+            res.send({errMsg:"数据库查询出错"});
+            return;
+        }
+        res.send(result);
+    })();
 
   
 })
